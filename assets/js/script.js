4e5b9171ef3e80a62cd564689cb1b1aee06388aa
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-03-20 15:26:22 -0400

    debug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,7 @@
-/*var questions = [
-    {
-        prompt: "Which statement cannot be used to declare a variable in JavaScript?\n(a) var\n\(b) let\n(c) int\n(d) const",
-        answer: "c"
-    },
-    {
-        prompt: "Which of the following is block scoped? (a) var  (b) let (c) const (d) Both b and c",
-        answer: "d"
-    },
-    {
-        prompt: "Which of the following is an array method? (a) map (b) filter (c) reduce (d) all of the above",
-        answer: "d"
-    },
-]*/
+//start quiz
 
-var score = 0;
 
 //quiz class
-
 class Quiz {
     constructor(questions) {
         this.score = 0;
@@ -49,7 +34,7 @@ class Question {
     isCorrectAnswer(choice) {
         return this.answer === choice;
     }
-}
+};
 
 // display question
 function displayQuestion() {
@@ -57,7 +42,7 @@ function displayQuestion() {
         showScores();
     }else{
 //show next question
-        let questionEl = document.getElementById("question");
+        let questionEl = document.getElementById("questions");
         questionEl.innerHTML = quiz.getQuestionIndex().text;
 
         //show options
@@ -78,38 +63,38 @@ function guess(id, guess) {
         quiz.guess(guess);
         displayQuestion();
     }
-}
+};
 // show quiz progress
 function showProgress() {
     let currentQuestionNumber = quiz.questionIndex + 1;
     let progressEl = document.getElementById("progress");
     progressEl.innerHTML = 
     `Question ${currentQuestionNumber} of ${quiz.questions.length}`;
-}
+};
 
 // show score
 function showScores() {
     let quizEndHTML = 
     `
     <h1> Quiz Completed</h1>
-    <h2 id="score">Your Score: ${quiz.score} of ${quiz.question.length}</h2>
+    <h2 id="score">Your Score: ${quiz.score} of ${quiz.questions.length}</h2>
     <div class="quiz-repeat">
     <a href="index.html">Take Quiz Again</a>
     </div>
     `;
     let quizEl = document.getElementById("quiz");
     quizEl.innerHTML = quizEndHTML;
-}
+};
 
 //quiz questions
 let questions = [
     new Question(
         "Which statement cannot be used to declare a variable in JavaScript?", 
-        ["var", "let", "int", "const"], "int"
+        ["var", "int","let", "const"], "int"
     ),
     new Question(
         "Which of the following is block scoped?", 
-        ["var", "let", "const", "Both b and c"], "Both b and c"
+        ["var", "let", "get", "Both 2 and 3"], "let"
         
     ),
     new Question(
@@ -123,3 +108,26 @@ let quiz = new Quiz(questions);
 
 //display question
 displayQuestion();
+
+//countdown
+let time = 2;
+let quizTimeInMinutes = time * 60 * 60;
+quizTime = quizTimeInMinutes / 60;
+
+let counting = document.getElementById("countdown");
+function startCountdown() {
+    let quizTimer = setInterval(function() {
+        if(quizTime <=0) {
+            clearInterval(quizTimer);
+            showScores();
+
+        }else{
+            quizTime--;
+            let sec = Math.floor(quizTime % 60);
+            let min = Math.floor(quizTime / 60)% 60;
+            counting.innerHTML = `TIME: ${min}:${sec}`;
+        }
+    }, 1000)
+};
+
+startCountdown();
